a2b1c8a29c6e7cd2b01b2cfb0a416ae608eb0df9
markyr96 <marioskyranas7@gmail.com>
2022-11-10 17:36:52 +0200

    end

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+function add7(number) {
+    console.log( number + 7);
+}
+
+add7 (4);
+
+function multiply(number1, number2) {
+    var mult = number1 * number2;
+    console.log(mult);
+}
+
+multiply(4, 5);
+
+function capitalize(string){
+    var cap1= (string[0].toUpperCase());
+    slicestring = string.slice(1, );
+    newstring = cap1 + slicestring;
+    console.log(newstring);   
+}
+
+capitalize("bumpkins");
+
+function lastLetter(string) {
+    var indexpos = string.length;
+    console.log(string[indexpos -1]);
+}
+
+lastLetter("Im here bro");
